70754ffc39dcc6214c7b056695e1f2e793d2e009
fabiannoda <angel.f.nodarse@hotmail.com>
2023-08-19 04:08:57 -0500

    Extracting stocks statistics

--- finance_scrap.py
+++ finance_scrap.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import pandas as pd
 import time
-from utilities import get_news
+from utilities import get_news, get_stocks
 
 options = Options()
 options.add_argument("--window-size=1920,1080")
-options.add_argument("start-maximized")
 
 web_url = 'https://www.google.com/finance/'
 service = Service(executable_path='./chromedriver.exe')
@@ -26,5 +29,32 @@ for clickable in clickable_news:
 
 final_news: pd.DataFrame = pd.concat(df_list)
 final_news.to_csv('./extracted_data/news.csv', index=False)
+
+driver.find_element(By.XPATH, '((//a[contains(@href,"/indexes")])[2]//span)[2]').click()
+
+new_url = driver.window_handles[0]
+driver.switch_to.window(new_url)
+
+time.sleep(3)
+
+clickable_stocks = driver.find_elements(By.XPATH, '//div[@role="navigation"]//div[@role="tablist"]//a')
+categories = [el.text for el in driver.find_elements(By.XPATH, '//div[@role="navigation"]//div[@role="tablist"]//a')]
+
+df_list = []
+
+for i in range(len(clickable_stocks)):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    element_xpath = f'(//div[@role="navigation"]//div[@role="tablist"]//a)[{i+1}]'
+    ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
+    element = WebDriverWait(driver, 3, ignored_exceptions=ignored_exceptions).until(expected_conditions.presence_of_element_located((By.XPATH, element_xpath)))
+    df_list.append(get_stocks(driver, categories[i]))
+    driver.execute_script("arguments[0].click();", element)
+    new_url = driver.window_handles[0]
+    driver.switch_to.window(new_url)
+    time.sleep(3)
+
+final_stocks: pd.DataFrame = pd.concat(df_list)
+final_stocks.to_csv('./extracted_data/stocks.csv', index=False)
+
 driver.quit()
 
--- utilities.py
+++ utilities.py
@@ -13,3 +13,25 @@ def get_news (driver: webdriver, category: str):
         link.append(new.get_attribute('href'))
     df = pd.DataFrame({'news_cast': news_cast, 'headline': headline, 'link': link, 'category': category})
     return df
+
+def get_stocks (driver: webdriver, category: str):
+    stock_abreviation = []
+    names = []
+    price = []
+    before_price_compare = []
+    percentage = []
+    stocks = driver.find_elements(By.XPATH, '//ul/li')
+
+    for stock in stocks:
+        stock_abreviation.append(stock.find_element(By.XPATH,'.//div[contains(@style,"background-color")]/div').text)
+        names.append(stock.find_element(By.XPATH,'.//div[2]/./div').text)
+        price.append(stock.find_element(By.XPATH,'.//span/./div/div').text)
+        before_price_compare.append(stock.find_element(By.XPATH,'.//div[3]/.//span').text)
+        percentage.append(stock.find_element(By.XPATH,'.//div[4]//span').get_attribute('aria-label'))
+    stock_abreviation = list(filter(None, stock_abreviation))
+    names = list(filter(None, names))
+    price = list(filter(None, price))
+    before_price_compare = list(filter(None, before_price_compare))
+    percentage = percentage[-len(names):len(percentage)]
+    df = pd.DataFrame({'stock_abreviation': stock_abreviation, 'names': names, 'price': price, 'before_price_compare': before_price_compare, 'percentage': percentage, 'category': category})
+    return df
